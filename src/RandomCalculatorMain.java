b72d3fc87efce9bb1b31eefc00356b3834238763
Qkite <yeonjigu4@gmail.com>
2022-10-05 11:15:08 +0900

    randomcalculator 생성

---
+++ src/RandomCalculatorMain.java
@@ -0,0 +1,12 @@
+public class RandomCalculatorMain {
+    public static void main(String[] args) {
+        RandomCalculator randomCalculator = new RandomCalculator(50);
+
+        randomCalculator.plus();
+        randomCalculator.minus();
+        randomCalculator.multiple();
+        randomCalculator.divide();
+
+    }
+
+}
