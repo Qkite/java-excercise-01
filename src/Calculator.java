9587c0c33e94f21b9f005764cf2e0b98fae706d9
Qkite <109712249+Qkite@users.noreply.github.com>
2022-10-05 10:48:41 +0900

    Update Calculator.java
    계산기 매개변수 제거

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,26 @@
 public class Calculator {
 
-    public static double plus(double num1, double num2){
+    static double num1, num2;
+
+    Calculator(double num1, double num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public static double plus(){
         return num1 + num2;
     }
 
-    public static double minus(double num1, double num2){
+    public static double minus(){
         return num1 - num2;
     }
 
 
-    public static double multiple(double num1, double num2){
+    public static double multiple(){
         return num1 * num2;
     }
 
-    public static double divide(double num1, double num2){
+    public static double divide(){
         return num1 / num2;
     }
 
