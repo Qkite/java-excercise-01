4090c63fd69b015e8654dea51cc545dc09edfb40
Qkite <yeonjigu4@gmail.com>
2022-10-05 10:02:14 +0900

    Initial commit

---
+++ src/SwapExcercise.java
@@ -0,0 +1,17 @@
+public class SwapExcercise {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        int temp=a;
+
+        // 숫자 바꾸기
+        a=b;
+        b=temp;
+
+        System.out.printf("a:%d b:%d", a,b);
+
+    }
+}
+Footer
+        © 2022 GitHub, I
