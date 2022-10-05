9ae8b52857a8f1bb78f74842bcadfb1fd9b1d230
Qkite <yeonjigu4@gmail.com>
2022-10-05 10:17:00 +0900

    계산기 업데이트

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public static double plus(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double minus(double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double multiple(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2){
+        return num1 / num2;
+    }
+
+}
