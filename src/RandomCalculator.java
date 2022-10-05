c2b79c6acb7fcbdf8807742fd626a54a64f8f3df
Qkite <109712249+Qkite@users.noreply.github.com>
2022-10-05 11:19:38 +0900

    randomcalculator 생성

---
+++ src/RandomCalculator.java
@@ -0,0 +1,31 @@
+public class RandomCalculator {
+
+    static int randomNumber = (int)(Math.random()*9);
+    static int inputNum;
+
+    RandomCalculator(int inputNum){
+        this.inputNum = inputNum;
+    }
+
+    public static void plus(){
+        System.out.println(inputNum + randomNumber);
+    }
+
+    public static void minus(){
+        System.out.println(inputNum - randomNumber);
+    }
+
+    public static void multiple(){
+        System.out.println(inputNum * randomNumber);
+    }
+
+    public static void divide(){
+        try{
+            System.out.println(inputNum + randomNumber);
+        }catch (ArithmeticException e){
+            System.out.println("random한 숫자가 0으로 생성되었습니다.");
+        }
+
+    }
+
+}
